1bc5e34e7ba5f8e40df79c711e30f28a248f6f08
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-03-01 10:20:25 +0100

    Initial work on collection for CodeUnit objects

---
+++ src/CodeUnitCollection.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/code-unit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\CodeUnit;
+
+final class CodeUnitCollection implements \Countable, \IteratorAggregate
+{
+    /**
+     * @var CodeUnit[]
+     */
+    private $codeUnits = [];
+
+    /**
+     * @param CodeUnit[] $items
+     */
+    public static function fromArray(array $items): self
+    {
+        $collection = new self;
+
+        foreach ($items as $item) {
+            $collection->add($item);
+        }
+
+        return $collection;
+    }
+
+    public static function fromList(CodeUnit ...$items): self
+    {
+        return self::fromArray($items);
+    }
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return CodeUnit[]
+     */
+    public function asArray(): array
+    {
+        return $this->codeUnits;
+    }
+
+    public function getIterator(): CodeUnitCollectionIterator
+    {
+        return new CodeUnitCollectionIterator($this);
+    }
+
+    public function count(): int
+    {
+        return \count($this->codeUnits);
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->codeUnits);
+    }
+
+    private function add(CodeUnit $item): void
+    {
+        $this->codeUnits[] = $item;
+    }
+}
---
+++ tests/unit/CodeUnitCollectionTest.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/code-unit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SebastianBergmann\CodeUnit;
+
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\CodeUnit\Fixture\FixtureClass;
+use SebastianBergmann\CodeUnit\Fixture\FixtureInterface;
+
+/**
+ * @covers \SebastianBergmann\CodeUnit\CodeUnitCollection
+ * @covers \SebastianBergmann\CodeUnit\CodeUnitCollectionIterator
+ *
+ * @uses \SebastianBergmann\CodeUnit\CodeUnit
+ *
+ * @testdox CodeUnitCollection
+ */
+final class CodeUnitCollectionTest extends TestCase
+{
+    /**
+     * @var InterfaceUnit
+     */
+    private $interface;
+
+    /**
+     * @var ClassUnit
+     */
+    private $class;
+
+    protected function setUp(): void
+    {
+        $this->interface = CodeUnit::forInterface(FixtureInterface::class);
+        $this->class     = CodeUnit::forClass(FixtureClass::class);
+    }
+
+    /**
+     * @testdox Can be created from array of CodeUnit objects
+     */
+    public function testCanBeCreatedFromArrayOfObjects(): void
+    {
+        $collection = CodeUnitCollection::fromArray([$this->interface, $this->class]);
+
+        $this->assertSame([$this->interface, $this->class], $collection->asArray());
+    }
+
+    /**
+     * @testdox Can be created from list of CodeUnit objects
+     */
+    public function testCanBeCreatedFromListOfObjects(): void
+    {
+        $collection = CodeUnitCollection::fromList($this->interface, $this->class);
+
+        $this->assertSame([$this->interface, $this->class], $collection->asArray());
+    }
+
+    public function testCanBeCounted(): void
+    {
+        $collection = CodeUnitCollection::fromList($this->interface, $this->class);
+
+        $this->assertCount(2, $collection);
+        $this->assertFalse($collection->isEmpty());
+    }
+
+    public function testCanBeIterated(): void
+    {
+        foreach (CodeUnitCollection::fromList($this->interface) as $key => $value) {
+            $this->assertSame(0, $key);
+            $this->assertSame($this->interface, $value);
+        }
+    }
+}
