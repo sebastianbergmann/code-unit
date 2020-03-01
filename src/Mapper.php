7110cbf1b5c13cc0161bba092ebfd38f4a97cd5c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-03-01 20:44:42 +0100

    Refactor

---
+++ src/Mapper.php
@@ -0,0 +1,251 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/code-unit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\CodeUnit;
+
+final class Mapper
+{
+    /**
+     * @psalm-return array<string,array<int,int>>
+     */
+    public function codeUnitsToSourceLines(CodeUnitCollection $codeUnits): array
+    {
+        $result = [];
+
+        foreach ($codeUnits as $codeUnit) {
+            $sourceFileName = $codeUnit->sourceFileName();
+
+            if (!isset($result[$sourceFileName])) {
+                $result[$sourceFileName] = [];
+            }
+
+            $result[$sourceFileName] = \array_merge($result[$sourceFileName], $codeUnit->sourceLines());
+        }
+
+        foreach (\array_keys($result) as $sourceFileName) {
+            $result[$sourceFileName] = \array_unique($result[$sourceFileName]);
+
+            \sort($result[$sourceFileName]);
+        }
+
+        \ksort($result);
+
+        return $result;
+    }
+
+    /**
+     * @throws InvalidCodeUnitException
+     * @throws ReflectionException
+     */
+    public function stringToCodeUnits(string $unit): CodeUnitCollection
+    {
+        if (\strpos($unit, '::') !== false) {
+            [$firstPart, $secondPart] = \explode('::', $unit);
+
+            if (empty($firstPart) && \function_exists($secondPart)) {
+                return CodeUnitCollection::fromList(CodeUnit::forFunction($secondPart));
+            }
+
+            if (\class_exists($firstPart)) {
+                if ($secondPart === '<public>') {
+                    return $this->publicMethodsOfClass($firstPart);
+                }
+
+                if ($secondPart === '<!public>') {
+                    return $this->protectedAndPrivateMethodsOfClass($firstPart);
+                }
+
+                if ($secondPart === '<protected>') {
+                    return $this->protectedMethodsOfClass($firstPart);
+                }
+
+                if ($secondPart === '<!protected>') {
+                    return $this->publicAndPrivateMethodsOfClass($firstPart);
+                }
+
+                if ($secondPart === '<private>') {
+                    return $this->privateMethodsOfClass($firstPart);
+                }
+
+                if ($secondPart === '<!private>') {
+                    return $this->publicAndProtectedMethodsOfClass($firstPart);
+                }
+
+                return CodeUnitCollection::fromList(CodeUnit::forClassMethod($firstPart, $secondPart));
+            }
+
+            if (\interface_exists($firstPart)) {
+                return CodeUnitCollection::fromList(CodeUnit::forInterfaceMethod($firstPart, $secondPart));
+            }
+
+            if (\trait_exists($firstPart)) {
+                return CodeUnitCollection::fromList(CodeUnit::forTraitMethod($firstPart, $secondPart));
+            }
+        } else {
+            if (\class_exists($unit)) {
+                $units = [CodeUnit::forClass($unit)];
+
+                foreach ($this->reflectorForClass($unit)->getTraits() as $trait) {
+                    $units[] = CodeUnit::forTrait($trait->getName());
+                }
+
+                return CodeUnitCollection::fromArray($units);
+            }
+
+            if (\interface_exists($unit)) {
+                return CodeUnitCollection::fromList(CodeUnit::forInterface($unit));
+            }
+
+            if (\trait_exists($unit)) {
+                return CodeUnitCollection::fromList(CodeUnit::forTrait($unit));
+            }
+
+            if (\function_exists($unit)) {
+                return CodeUnitCollection::fromList(CodeUnit::forFunction($unit));
+            }
+
+            if (\strpos($unit, '<extended>') !== false) {
+                $unit = \str_replace('<extended>', '', $unit);
+
+                if (\class_exists($unit)) {
+                    return $this->classAndParentClasses($unit);
+                }
+            }
+        }
+
+        throw new InvalidCodeUnitException(
+            \sprintf(
+                '"%s" is not a valid code unit',
+                $unit
+            )
+        );
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function publicMethodsOfClass(string $className): CodeUnitCollection
+    {
+        return $this->methodsOfClass($className, \ReflectionMethod::IS_PUBLIC);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function publicAndProtectedMethodsOfClass(string $className): CodeUnitCollection
+    {
+        return $this->methodsOfClass($className, \ReflectionMethod::IS_PUBLIC | \ReflectionMethod::IS_PROTECTED);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function publicAndPrivateMethodsOfClass(string $className): CodeUnitCollection
+    {
+        return $this->methodsOfClass($className, \ReflectionMethod::IS_PUBLIC | \ReflectionMethod::IS_PRIVATE);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function protectedMethodsOfClass(string $className): CodeUnitCollection
+    {
+        return $this->methodsOfClass($className, \ReflectionMethod::IS_PROTECTED);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function protectedAndPrivateMethodsOfClass(string $className): CodeUnitCollection
+    {
+        return $this->methodsOfClass($className, \ReflectionMethod::IS_PROTECTED | \ReflectionMethod::IS_PRIVATE);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function privateMethodsOfClass(string $className): CodeUnitCollection
+    {
+        return $this->methodsOfClass($className, \ReflectionMethod::IS_PRIVATE);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function methodsOfClass(string $className, int $filter): CodeUnitCollection
+    {
+        $units = [];
+
+        foreach ($this->reflectorForClass($className)->getMethods($filter) as $method) {
+            $units[] = CodeUnit::forClassMethod($className, $method->getName());
+        }
+
+        return CodeUnitCollection::fromArray($units);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function classAndParentClasses(string $className): CodeUnitCollection
+    {
+        $units = [CodeUnit::forClass($className)];
+
+        $reflector = $this->reflectorForClass($className);
+
+        foreach ($this->reflectorForClass($className)->getTraits() as $trait) {
+            $units[] = CodeUnit::forTrait($trait->getName());
+        }
+
+        while ($reflector = $reflector->getParentClass()) {
+            $units[] = CodeUnit::forClass($reflector->getName());
+
+            foreach ($this->reflectorForClass($className)->getTraits() as $trait) {
+                $units[] = CodeUnit::forTrait($trait->getName());
+            }
+        }
+
+        return CodeUnitCollection::fromArray($units);
+    }
+
+    /**
+     * @psalm-param class-string $className
+     *
+     * @throws ReflectionException
+     */
+    private function reflectorForClass(string $className): \ReflectionClass
+    {
+        try {
+            return new \ReflectionClass($className);
+            // @codeCoverageIgnoreStart
+        } catch (\ReflectionException $e) {
+            throw new ReflectionException(
+                $e->getMessage(),
+                (int) $e->getCode(),
+                $e
+            );
+        }
+        // @codeCoverageIgnoreEnd
+    }
+}
---
+++ tests/unit/MapperTest.php
@@ -0,0 +1,225 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/code-unit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\CodeUnit;
+
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\CodeUnit\Fixture\FixtureClass;
+use SebastianBergmann\CodeUnit\Fixture\FixtureClassWithTrait;
+use SebastianBergmann\CodeUnit\Fixture\FixtureInterface;
+use SebastianBergmann\CodeUnit\Fixture\FixtureParentClass;
+use SebastianBergmann\CodeUnit\Fixture\FixtureTrait;
+
+/**
+ * @covers \SebastianBergmann\CodeUnit\Mapper
+ *
+ * @uses \SebastianBergmann\CodeUnit\CodeUnit
+ * @uses \SebastianBergmann\CodeUnit\CodeUnitCollection
+ * @uses \SebastianBergmann\CodeUnit\CodeUnitCollectionIterator
+ */
+final class MapperTest extends TestCase
+{
+    /**
+     * @testdox Can map 'function_name' string to code unit objects
+     */
+    public function testCanMapStringWithFunctionNameToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits('SebastianBergmann\CodeUnit\Fixture\f');
+
+        $this->assertSame('SebastianBergmann\CodeUnit\Fixture\f', $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map '::function_name' string to code unit objects
+     */
+    public function testCanMapStringWithFunctionNamePrefixedWithDoubleColonsToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits('::f');
+
+        $this->assertSame('f', $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class);
+
+        $this->assertCount(1, $units);
+        $this->assertSame(FixtureClass::class, $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName' string to code unit objects
+     */
+    public function testCanBeCreatedForClassesAndTheTraitsTheyUseToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClassWithTrait::class);
+
+        $this->assertCount(2, $units);
+        $this->assertSame(FixtureClassWithTrait::class, $units->asArray()[0]->name());
+        $this->assertSame(FixtureTrait::class, $units->asArray()[1]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName<extended>' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndSelectorForParentClassesToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '<extended>');
+
+        $this->assertCount(2, $units);
+        $this->assertSame(FixtureClass::class, $units->asArray()[0]->name());
+        $this->assertSame(FixtureParentClass::class, $units->asArray()[1]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName::methodName' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndMethodNameToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '::publicMethod');
+
+        $this->assertSame(FixtureClass::class . '::publicMethod', $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName::<public>' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndPublicMethodSelectorToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '::<public>');
+
+        $this->assertCount(1, $units);
+        $this->assertSame(FixtureClass::class . '::publicMethod', $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName::<!public>' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndNoPublicMethodSelectorToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '::<!public>');
+
+        $this->assertCount(2, $units);
+        $this->assertSame(FixtureClass::class . '::protectedMethod', $units->asArray()[0]->name());
+        $this->assertSame(FixtureClass::class . '::privateMethod', $units->asArray()[1]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName::<protected>' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndProtectedMethodSelectorToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '::<protected>');
+
+        $this->assertCount(1, $units);
+        $this->assertSame(FixtureClass::class . '::protectedMethod', $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName::<!protected>' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndNoProtectedMethodSelectorToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '::<!protected>');
+
+        $this->assertCount(2, $units);
+        $this->assertSame(FixtureClass::class . '::publicMethod', $units->asArray()[0]->name());
+        $this->assertSame(FixtureClass::class . '::privateMethod', $units->asArray()[1]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName::<private>' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndPrivateMethodSelectorToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '::<private>');
+
+        $this->assertCount(1, $units);
+        $this->assertSame(FixtureClass::class . '::privateMethod', $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'ClassName::<!private>' string to code unit objects
+     */
+    public function testCanMapStringWithClassNameAndNoPrivateMethodSelectorToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureClass::class . '::<!private>');
+
+        $this->assertCount(2, $units);
+        $this->assertSame(FixtureClass::class . '::publicMethod', $units->asArray()[0]->name());
+        $this->assertSame(FixtureClass::class . '::protectedMethod', $units->asArray()[1]->name());
+    }
+
+    /**
+     * @testdox Can map 'InterfaceName' string to code unit objects
+     */
+    public function testCanMapStringWithInterfaceNameToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureInterface::class);
+
+        $this->assertSame(FixtureInterface::class, $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'InterfaceName::methodName' string to code unit objects
+     */
+    public function testCanMapStringWithInterfaceNameAndMethodNameToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureInterface::class . '::method');
+
+        $this->assertSame(FixtureInterface::class . '::method', $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'TraitName' string to code unit objects
+     */
+    public function testCanMapStringWithTraitNameToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureTrait::class);
+
+        $this->assertSame(FixtureTrait::class, $units->asArray()[0]->name());
+    }
+
+    /**
+     * @testdox Can map 'TraitName::methodName' string to code unit objects
+     */
+    public function testCanMapStringWithTraitNameAndMethodNameToCodeUnitObjects(): void
+    {
+        $units = (new Mapper)->stringToCodeUnits(FixtureTrait::class . '::method');
+
+        $this->assertSame(FixtureTrait::class . '::method', $units->asArray()[0]->name());
+    }
+
+    public function testCannotMapInvalidStringToCodeUnitObjects(): void
+    {
+        $this->expectException(InvalidCodeUnitException::class);
+
+        (new Mapper)->stringToCodeUnits('invalid');
+    }
+
+    public function testCanMapCodeUnitObjectsToArrayWithSourceLines(): void
+    {
+        $codeUnits = CodeUnitCollection::fromList(
+            CodeUnit::forInterface(FixtureInterface::class),
+            CodeUnit::forClass(FixtureClass::class),
+            CodeUnit::forClass(FixtureClass::class)
+        );
+
+        $this->assertSame(
+            [
+                \realpath(__DIR__ . '/../_fixture/FixtureClass.php')     => \range(12, 28),
+                \realpath(__DIR__ . '/../_fixture/FixtureInterface.php') => \range(12, 15),
+            ],
+            (new Mapper)->codeUnitsToSourceLines($codeUnits)
+        );
+    }
+}
